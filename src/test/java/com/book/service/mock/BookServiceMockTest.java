d5b72122d6b4c70e8935e84cd0884ae8ea5ffb8f
Hiren Savalia <hirensavalia.879@gmail.com>
2021-10-16 23:43:40 +0530

    Handling exception and adding test case for it

---
+++ src/test/java/com/book/service/mock/BookServiceMockTest.java
@@ -0,0 +1,55 @@
+package com.book.service.mock;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.book.exception.BookException;
+import com.book.service.BookInfoService;
+import com.book.service.BookService;
+import com.book.service.ReviewService;
+
+import reactor.test.StepVerifier;
+
+@ExtendWith(MockitoExtension.class)
+public class BookServiceMockTest {
+
+	@Mock
+	private BookInfoService bookInfoService;
+
+	@Mock
+	private ReviewService reviewService;
+
+	@InjectMocks
+	private BookService bookService;
+
+	@Test
+	void getBooksMock() {
+
+		Mockito.when(bookInfoService.getBookInfo()).thenCallRealMethod();
+
+		Mockito.when(reviewService.getBookReview(Mockito.anyLong())).thenCallRealMethod();
+
+		var books = bookService.getBooks();
+
+		StepVerifier.create(books).expectNextCount(3).verifyComplete();
+
+	}
+
+	@Test
+	void getBooksMockException() {
+
+		Mockito.when(bookInfoService.getBookInfo()).thenCallRealMethod();
+
+		Mockito.when(reviewService.getBookReview(Mockito.anyLong()))
+				.thenThrow(new IllegalStateException("Exception from Mock test"));
+
+		var books = bookService.getBooks();
+
+		StepVerifier.create(books).expectError(BookException.class).verify();
+
+	}
+}
